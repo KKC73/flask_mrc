2dfd3b76f824027f1b320bcbe5e71406bbd84f83
KKC73 <50138914+KKC73@users.noreply.github.com>
2023-02-21 16:03:48 +0700

    Nothing

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import db
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
+# from flask_restful import Resource, Api, reqparse
+# import pandas as pd
+# import ast
 
-# Fetch the service account key JSON file contents
-cred = credentials.Certificate('mekong-6edd6-firebase-adminsdk-14sqd-df1a18f32f.json')
-# Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://mekong-6edd6-default-rtdb.firebaseio.com'
-})
+# # Fetch the service account key JSON file contents
+# cred = credentials.Certificate('mekong-6edd6-firebase-adminsdk-14sqd-df1a18f32f.json')
+# # Initialize the app with a service account, granting admin privileges
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://mekong-6edd6-default-rtdb.firebaseio.com'
+# })
 
-ref = db.reference('/')
+# ref = db.reference('/')
 
-ref = db.reference('boxes')
+# ref = db.reference('boxes')
 
-app = Flask(__name__)
-api = Api(app)
+# app = Flask(__name__)
+# api = Api(app)
 
-class MRC(Resource):
-    def get(self):
-        return ref.get(), 200  # return data and 200 OK
+# class Index(Resource):
+#     def get(self):
+#         return {"Message: ": "Welcome!"}, 200  # return data and 200 OK
+# class MRC(Resource):
+#     def get(self):
+#         return ref.get(), 200  # return data and 200 OK
 
-api.add_resource(MRC, '/mrc')  # add endpoints
+# api.add_resource(MRC, '/mrc')  # add endpoints
+# api.add_resource(Index, '/')  # add endpoints
 
-if __name__ == '__main__':
-    app.run()  # run our Flask app
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)  # run our Flask app
+
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    return "Your Flask App Works!"
+
+@application.route("/hello")
+def hello():
+    return "Hello World!"
+
+# @application.route("/mrc")
+# def hello():
+#     return ref.get()
+
+
+if __name__ == "__main__":
+    application.run(port=5000, debug=True)
